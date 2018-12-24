d7811ed1041ce04a04bcebbaf1952e80611a5d48
DR <danielrubak@gmail.com>
2018-12-24 13:15:48 +0100

    Add tests for all methods of Matrix class

--- lab03/src/Matrix.java
+++ lab03/src/Matrix.java
@@ -1,14 +1,19 @@
+import java.util.Random;
+
 public class Matrix {
-    double[]data;
+    double[] data;
     int rows;
     int cols;
 
-    Matrix(int rows, int cols){
+    Matrix(int rows, int cols) {
         this.rows = rows;
         this.cols = cols;
-        data = new double[rows*cols];
+        data = new double[rows * cols];
     }
 
+    /**
+     * @param d - two dimensional double array, max length of row is a number of columns
+     */
     public Matrix(double[][] d) {
         this.rows = d.length;
 
@@ -22,7 +27,7 @@ public class Matrix {
 
         for (int i = 0; i < this.rows; i++) {
             for (int j = 0; j < this.cols; j++) {
-                if ( j > (d[i].length - 1)) {
+                if (j > (d[i].length - 1)) {
                     this.data[i * this.cols + j] = 0;
                 } else {
                     this.data[i * this.cols + j] = d[i][j];
@@ -31,6 +36,9 @@ public class Matrix {
         }
     }
 
+    /**
+     * @return two dimensional double array
+     */
     public double[][] asArray() {
         double resultMatrix[][] = new double[this.rows][this.cols];
         for (int i = 0; i < this.rows; i++) {
@@ -41,22 +49,35 @@ public class Matrix {
         return resultMatrix;
     }
 
-    double get(int r,int c) {
+    /**
+     * @param r - row index
+     * @param c - column index
+     * @return value from data[r][c]
+     */
+    double get(int r, int c) {
         return this.data[this.cols * r + c];
     }
 
-    void set (int r,int c, double value) {
+    /**
+     * @param r - row index
+     * @param c - column index
+     * @param value - setting value to data[r][c]
+     */
+    void set(int r, int c, double value) {
         this.data[this.cols * r + c] = value;
     }
 
+    /**
+     * @return data from this.data as an array formatted as string
+     */
     public String toString() {
         StringBuilder buf = new StringBuilder();
         buf.append("[");
 
-        for (int i=0; i<this.rows; i++) {
+        for (int i = 0; i < this.rows; i++) {
             buf.append("[");
             int j = 0;
-            for (; j<this.cols-1; j++) {
+            for (; j < this.cols - 1; j++) {
                 buf.append(this.data[i * this.cols + j]);
                 buf.append(", ");
             }
@@ -64,21 +85,28 @@ public class Matrix {
             buf.append("]\n");
         }
 
-        buf.deleteCharAt(buf.length()-1);
+        buf.deleteCharAt(buf.length() - 1);
         buf.append("]");
 
         return buf.toString();
     }
 
-    void reshape (int newRows,int newCols) {
-        if(rows*cols != newRows*newCols)
-            throw new RuntimeException(String.format("%d x %d matrix can't be reshaped to %d x %d",rows,cols,newRows,newCols));
+    /**
+     * @param newRows - new number of rows
+     * @param newCols - new number of columns
+     */
+    void reshape(int newRows, int newCols) {
+        if (rows * cols != newRows * newCols)
+            throw new RuntimeException(String.format("%d x %d matrix can't be reshaped to %d x %d", rows, cols, newRows, newCols));
         else {
             this.cols = newCols;
             this.rows = newRows;
         }
     }
 
+    /**
+     * @return vector which contain number of rows on index 0 and number of columns on index 1
+     */
     public int[] shape() {
         int[] shape = new int[2];
         shape[0] = rows;
@@ -87,14 +115,18 @@ public class Matrix {
         return shape;
     }
 
-    public Matrix add (Matrix m) {
+    /**
+     * @param m - Matrix object to add to this
+     * @return result of matrices adding
+     */
+    public Matrix add(Matrix m) {
         if (this.shape()[0] != m.shape()[0] || this.shape()[1] != m.shape()[1])
             throw new RuntimeException("Matrices differ in dimensions");
 
         Matrix resultMatrix = new Matrix(this.rows, this.cols);
         double[] tempMatrix = this.data.clone();
 
-        for (int i=0; i<tempMatrix.length; i++) {
+        for (int i = 0; i < tempMatrix.length; i++) {
             tempMatrix[i] += m.data[i];
         }
 
@@ -102,57 +134,15 @@ public class Matrix {
         return resultMatrix;
     }
 
-    public Matrix sub (Matrix m) {
-        if (this.shape()[0] != m.shape()[0] || this.shape()[1] != m.shape()[1])
-            throw new RuntimeException("Matrices differ in dimensions");
-
+    /**
+     * @param w - value to add to every element of this object
+     * @return result of matrices adding
+     */
+    public Matrix add(double w) {
         Matrix resultMatrix = new Matrix(this.rows, this.cols);
         double[] tempMatrix = this.data.clone();
 
-        for (int i=0; i<tempMatrix.length; i++) {
-            tempMatrix[i] -= m.data[i];
-        }
-
-        resultMatrix.data = tempMatrix;
-        return resultMatrix;
-    }
-
-    public Matrix mul (Matrix m) {
-
-        if (this.shape()[0] != m.shape()[0] || this.shape()[1] != m.shape()[1])
-            throw new RuntimeException("Matrices differ in dimensions");
-
-        Matrix resultMatrix = new Matrix(this.rows, this.cols);
-        double[] tempMatrix = this.data.clone();
-
-        for (int i=0; i<tempMatrix.length; i++) {
-            tempMatrix[i] *= m.data[i];
-        }
-
-        resultMatrix.data = tempMatrix;
-        return resultMatrix;
-    }
-
-    public Matrix div (Matrix m) {
-        if (this.shape()[0] != m.shape()[0] || this.shape()[1] != m.shape()[1])
-            throw new RuntimeException("Matrices differ in dimensions");
-
-        Matrix resultMatrix = new Matrix(this.rows, this.cols);
-        double[] tempMatrix = this.data.clone();
-
-        for(int i=0; i<tempMatrix.length; i++) {
-            tempMatrix[i] /= m.data[i];
-        }
-
-        resultMatrix.data = tempMatrix;
-        return resultMatrix;
-    }
-
-    public Matrix add (double w) {
-        Matrix resultMatrix = new Matrix(this.rows, this.cols);
-        double[] tempMatrix = this.data.clone();
-
-        for (int i=0; i<tempMatrix.length; i++) {
+        for (int i = 0; i < tempMatrix.length; i++) {
             tempMatrix[i] += w;
         }
 
@@ -160,11 +150,34 @@ public class Matrix {
         return resultMatrix;
     }
 
-    public Matrix sub (double w) {
+    /**
+     * @param m - Matrix object to subtract from this
+     * @return result of matrices subtraction
+     */
+    public Matrix sub(Matrix m) {
+        if (this.shape()[0] != m.shape()[0] || this.shape()[1] != m.shape()[1])
+            throw new RuntimeException("Matrices differ in dimensions");
+
         Matrix resultMatrix = new Matrix(this.rows, this.cols);
         double[] tempMatrix = this.data.clone();
 
-        for (int i=0; i<tempMatrix.length; i++) {
+        for (int i = 0; i < tempMatrix.length; i++) {
+            tempMatrix[i] -= m.data[i];
+        }
+
+        resultMatrix.data = tempMatrix;
+        return resultMatrix;
+    }
+
+    /**
+     * @param w - value to subtract from every element of this object
+     * @return result of matrices subtraction
+     */
+    public Matrix sub(double w) {
+        Matrix resultMatrix = new Matrix(this.rows, this.cols);
+        double[] tempMatrix = this.data.clone();
+
+        for (int i = 0; i < tempMatrix.length; i++) {
             tempMatrix[i] -= w;
         }
 
@@ -172,11 +185,33 @@ public class Matrix {
         return resultMatrix;
     }
 
-    public Matrix mul (double w) {
+    /**
+     * @param m - Matrix object to multiply with this (only items on the same index are multiplied)
+     * @return result of matrices multiplication
+     */
+    public Matrix mul(Matrix m) {
+
+        if (this.shape()[0] != m.shape()[0] || this.shape()[1] != m.shape()[1])
+            throw new RuntimeException("Matrices differ in dimensions");
+
+        Matrix resultMatrix = new Matrix(this.rows, this.cols);
+        double[] newData = this.data.clone();
+        for (int i = 0; i < newData.length; i++) {
+            newData[i] *= m.data[i];
+        }
+        resultMatrix.data = newData;
+        return resultMatrix;
+    }
+
+    /**
+     * @param w - value by which all elements are to be multiplied
+     * @return result of matrices multiplication
+     */
+    public Matrix mul(double w) {
         Matrix resultMatrix = new Matrix(this.rows, this.cols);
         double[] tempMatrix = this.data.clone();
 
-        for (int i=0; i<tempMatrix.length; i++) {
+        for (int i = 0; i < tempMatrix.length; i++) {
             tempMatrix[i] *= w;
         }
 
@@ -184,11 +219,38 @@ public class Matrix {
         return resultMatrix;
     }
 
-    public Matrix div (double w) {
+    /**
+     * @param m - Matrix object to divide with this (only items on the same index are divided)
+     * @return result of matrices division
+     */
+    public Matrix div(Matrix m) {
+        if (this.shape()[0] != m.shape()[0] || this.shape()[1] != m.shape()[1])
+            throw new RuntimeException("Matrices differ in dimensions");
+
         Matrix resultMatrix = new Matrix(this.rows, this.cols);
         double[] tempMatrix = this.data.clone();
 
-        for(int i=0; i<tempMatrix.length; i++) {
+        for (int i = 0; i < tempMatrix.length; i++) {
+            if ( m.data[i] == 0 ) {
+                tempMatrix[i] = 0;
+            } else {
+                tempMatrix[i] /= m.data[i];
+            }
+        }
+
+        resultMatrix.data = tempMatrix;
+        return resultMatrix;
+    }
+
+    /**
+     * @param w - value by which all elements are to be divided
+     * @return result of matrices division
+     */
+    public Matrix div(double w) {
+        Matrix resultMatrix = new Matrix(this.rows, this.cols);
+        double[] tempMatrix = this.data.clone();
+
+        for (int i = 0; i < tempMatrix.length; i++) {
             tempMatrix[i] /= w;
         }
 
@@ -196,16 +258,20 @@ public class Matrix {
         return resultMatrix;
     }
 
-    public Matrix dot (Matrix other) {
+    /**
+     * @param other - Matrix object to multiply with this
+     * @return result of matrices multiplication
+     */
+    public Matrix dot(Matrix other) {
         if (this.rows != other.cols || this.cols != other.rows)
             throw new RuntimeException("Matrices should have sizes: n x m and m x n");
 
-        Matrix resultMatrix =  new Matrix(2,2);
+        Matrix resultMatrix = new Matrix(2, 2);
 
-        for (int i=0; i < this.rows; i++) {
-            for (int j=0; j < other.cols; j++) {
+        for (int i = 0; i < this.rows; i++) {
+            for (int j = 0; j < other.cols; j++) {
                 double sum = 0;
-                for (int k=0; k < this.cols; k++) {
+                for (int k = 0; k < this.cols; k++) {
                     sum += this.get(i, k) * other.get(k, j);
                 }
                 resultMatrix.set(i, j, sum);
@@ -214,26 +280,75 @@ public class Matrix {
         return resultMatrix;
     }
 
+    /**
+     * @return frobenius norm
+     */
     public double frobenius() {
         double sum = 0;
-
-        for (int i=0; i<this.data.length; i++) {
+        for ( int i = 0; i < this.data.length; i++ ) {
             sum += data[i] * data[i];
         }
-
         return Math.sqrt(sum);
     }
 
-    // KOLOKWIUM
+    /**
+     * @param rows - number of rows
+     * @param cols - number of columns
+     * @return matrix of size rowsxcols filled with random values
+     */
+    public static Matrix random(int rows, int cols){
+        Matrix m = new Matrix(rows,cols);
+        Random r = new Random();
+        for ( int i = 0; i < rows; i++ ) {
+            for ( int j = 0; j < cols; j++ ) {
+                m.set(i, j, r.nextDouble());
+            }
+        }
+        return m;
+    }
+
+    /**
+     * @param n - matrix size
+     * @return the unit matrix of size nxn
+     */
+    public static Matrix eye(int n){
+        Matrix m = new Matrix(n,n);
+        for ( int i = 0; i < n; i++ ) {
+            for ( int j = 0; j < n; j++ ) {
+                if ( i == j ) {
+                    m.set(i, j,1);
+                } else {
+                    m.set(i, j,0);
+                }
+            }
+        }
+        return m;
+    }
+
+    /**
+     * @param rows - number of rows
+     * @param cols - number of columns
+     * @return matrix of size rows X cols filled with zeros
+     */
+    public static Matrix zeros(int rows, int cols) {
+        Matrix m = new Matrix(rows,cols);
+        for ( int i = 0; i < rows; i++ ) {
+            for ( int j = 0; j < cols; j++ ) {
+                m.set(i, j, 0);
+            }
+        }
+        return m;
+    }
+
+    // ---------------------- //
+    // Task on the colloquium //
+    // ---------------------- //
+
     public Matrix sumRows() {
-        Matrix resultMatrix =  new Matrix(1, this.rows);
-
-        System.out.println(this.rows);
-        System.out.println(this.cols);
-
+        Matrix resultMatrix = new Matrix(1, this.rows);
         for (int i = 0; i < this.cols; i++) {
             double sum = 0;
-            for ( int j = 0; j < this.rows; j++) {
+            for (int j = 0; j < this.rows; j++) {
                 sum += this.data[j * this.cols + i];
             }
             resultMatrix.data[i] = sum;
--- lab03/test/MatrixTest.java
+++ lab03/test/MatrixTest.java
@@ -84,45 +84,124 @@ public class MatrixTest {
 
     @org.junit.Test
     public void shape() {
+        Matrix matrixForTest = generate2X3Matrix();
+        assertEquals(2, matrixForTest.shape()[0], .2);
+        assertEquals(3, matrixForTest.shape()[1], .2);
     }
 
     @org.junit.Test
     public void add() {
-    }
-
-    @org.junit.Test
-    public void sub() {
-    }
-
-    @org.junit.Test
-    public void mul() {
-    }
-
-    @org.junit.Test
-    public void div() {
+        Matrix matrixForTest = generate2X2Matrix();
+        Matrix result = matrixForTest.add(Matrix.eye(2));
+        assertEquals(result.get(0, 0), 3, .2);
+        assertEquals(result.get(0, 1), 4, .2);
+        assertEquals(result.get(1, 0), 6, .2);
+        assertEquals(result.get(1, 1), 9, .2);
     }
 
     @org.junit.Test
     public void add1() {
+        Matrix matrixForTest = generate2X2Matrix();
+        Matrix result = matrixForTest.add(1);
+        assertEquals(result.get(0, 0), 3, .2);
+        assertEquals(result.get(0, 1), 5, .2);
+        assertEquals(result.get(1, 0), 7, .2);
+        assertEquals(result.get(1, 1), 9, .2);
+    }
+
+    @org.junit.Test
+    public void sub() {
+        Matrix matrixForTest = generate2X2Matrix();
+        Matrix result = matrixForTest.sub(Matrix.eye(2));
+        assertEquals(result.get(0, 0), 1, .2);
+        assertEquals(result.get(0, 1), 4, .2);
+        assertEquals(result.get(1, 0), 6, .2);
+        assertEquals(result.get(1, 1), 7, .2);
     }
 
     @org.junit.Test
     public void sub1() {
+        Matrix matrixForTest = generate2X2Matrix();
+        Matrix result = matrixForTest.sub(1);
+        assertEquals(result.get(0, 0), 1, .2);
+        assertEquals(result.get(0, 1), 3, .2);
+        assertEquals(result.get(1, 0), 5, .2);
+        assertEquals(result.get(1, 1), 7, .2);
+    }
+
+    @org.junit.Test
+    public void mul() {
+        Matrix matrixForTest = generate2X2Matrix();
+        Matrix result = matrixForTest.mul(Matrix.eye(2));
+        assertEquals(result.get(0, 0), 2, .2);
+        assertEquals(result.get(0, 1), 0, .2);
+        assertEquals(result.get(1, 0), 0, .2);
+        assertEquals(result.get(1, 1), 8, .2);
     }
 
     @org.junit.Test
     public void mul1() {
+        Matrix matrixForTest = generate2X2Matrix();
+        Matrix result = matrixForTest.mul(2);
+        assertEquals(result.get(0, 0), 4, .2);
+        assertEquals(result.get(0, 1), 8, .2);
+        assertEquals(result.get(1, 0), 12, .2);
+        assertEquals(result.get(1, 1), 16, .2);
+    }
+
+    @org.junit.Test
+    public void div() {
+        Matrix matrixForTest = generate2X2Matrix();
+        Matrix result = matrixForTest.div(Matrix.eye(2));
+        assertEquals(result.get(0, 0), 2, .2);
+        assertEquals(result.get(0, 1), 0, .2);
+        assertEquals(result.get(1, 0), 0, .2);
+        assertEquals(result.get(1, 1), 8, .2);
     }
 
     @org.junit.Test
     public void div1() {
+        Matrix matrixForTest = generate2X2Matrix();
+        Matrix result = matrixForTest.div(2);
+        assertEquals(result.get(0, 0), 1, .2);
+        assertEquals(result.get(0, 1), 2, .2);
+        assertEquals(result.get(1, 0), 3, .2);
+        assertEquals(result.get(1, 1), 4, .2);
     }
 
     @org.junit.Test
     public void dot() {
+        Matrix matrixForTest1 = generate2X3Matrix();
+        Matrix matrixForTest2 = generate2X3Matrix();
+        matrixForTest2.reshape(3,2);
+        Matrix result = matrixForTest1.dot(matrixForTest2);
+        assertEquals(result.get(0, 0), 22, .2);
+        assertEquals(result.get(0, 1), 28, .2);
+        assertEquals(result.get(1, 0), 49, .2);
+        assertEquals(result.get(1, 1), 64, .2);
     }
 
     @org.junit.Test
     public void frobenius() {
+        Matrix matrixForTest = generate2X2Matrix();
+        assertEquals(10.95, matrixForTest.frobenius(), .1);
+    }
+
+    @org.junit.Test
+    public void zeros() {
+        Matrix matrixForTest = Matrix.zeros(2, 2);
+        assertEquals(0, matrixForTest.get(0, 0), .2);
+        assertEquals(0, matrixForTest.get(0, 1), .2);
+        assertEquals(0, matrixForTest.get(1, 0), .2);
+        assertEquals(0, matrixForTest.get(1, 1), .2);
+    }
+
+    @org.junit.Test
+    public void eye() {
+        Matrix matrixForTest = Matrix.eye(2);
+        assertEquals(1, matrixForTest.get(0, 0), .2);
+        assertEquals(0, matrixForTest.get(0, 1), .2);
+        assertEquals(0, matrixForTest.get(1, 0), .2);
+        assertEquals(1, matrixForTest.get(1, 1), .2);
     }
 }
