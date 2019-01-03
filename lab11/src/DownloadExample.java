87ab94062775f73d2f21f80a0fdd4ee89874fd78
DR <danielrubak@gmail.com>
2019-01-03 18:57:44 +0100

    Add Clock, ClockWithGui and DownloadExample classes, threads operations

---
+++ lab11/src/DownloadExample.java
@@ -0,0 +1,81 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Locale;
+import java.util.concurrent.Semaphore;
+
+public class DownloadExample {
+    static Semaphore sem;
+
+    // lista plików do pobrania
+    static String[] toDownload = {
+            "http://home.agh.edu.pl/pszwed/wyklad-c/01-jezyk-c-intro.pdf",
+            "http://home.agh.edu.pl/~pszwed/wyklad-c/02-jezyk-c-podstawy-skladni.pdf",
+            "http://home.agh.edu.pl/~pszwed/wyklad-c/03-jezyk-c-instrukcje.pdf",
+            "http://home.agh.edu.pl/~pszwed/wyklad-c/04-jezyk-c-funkcje.pdf",
+            "http://home.agh.edu.pl/~pszwed/wyklad-c/05-jezyk-c-deklaracje-typy.pdf",
+            "http://home.agh.edu.pl/~pszwed/wyklad-c/06-jezyk-c-wskazniki.pdf",
+            "http://home.agh.edu.pl/~pszwed/wyklad-c/07-jezyk-c-operatory.pdf",
+            "http://home.agh.edu.pl/~pszwed/wyklad-c/08-jezyk-c-lancuchy-znakow.pdf",
+            "http://home.agh.edu.pl/~pszwed/wyklad-c/09-jezyk-c-struktura-programow.pdf",
+            "http://home.agh.edu.pl/~pszwed/wyklad-c/10-jezyk-c-dynamiczna-alokacja-pamieci.pdf",
+            "http://home.agh.edu.pl/~pszwed/wyklad-c/11-jezyk-c-biblioteka-we-wy.pdf",
+            "http://home.agh.edu.pl/~pszwed/wyklad-c/preprocesor-make-funkcje-biblioteczne.pdf",
+    };
+
+    static class Downloader implements Runnable {
+        private final String url;
+
+        Downloader(String url) {
+            this.url = url;
+        }
+
+        public void run() {
+            String fileName = url.substring(url.lastIndexOf("/")+1);
+
+            try (InputStream in = new URL(url).openStream(); FileOutputStream out = new FileOutputStream(fileName)) {
+                while (true) {
+                    int c = in.read();
+                    if (c < 0) break;
+                    out.write(c);
+                }
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Done:" + fileName);
+            sem.release();
+        }
+
+    }
+
+    static void sequentialDownload(){
+        sem = new Semaphore(0);
+        double t1 = System.nanoTime()/1e6;
+        for(String url:toDownload){
+            new Downloader(url).run();
+        }
+        double t2 = System.nanoTime()/1e6;
+        System.out.printf(Locale.US,"t2-t1=%f\n",t2-t1);
+    }
+
+    static void concurrentDownload() throws InterruptedException {
+        sem = new Semaphore(0);
+        double t1 = System.nanoTime()/1e6;
+        for(String url:toDownload){
+            new Thread(new Downloader(url)).start();
+        }
+        sem.acquire(toDownload.length);
+        double t2 = System.nanoTime()/1e6;
+        System.out.printf(Locale.US,"t2-t1=%f\n",t2-t1);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        sequentialDownload();
+        concurrentDownload();
+    }
+}
